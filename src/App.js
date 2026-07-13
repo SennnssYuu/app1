a232ba80795c54080a0d47e99b29fbbab02a52b4
Sennnss <marionvam723@gmail.com>
2026-07-13 17:40:17 +0700

    Update App.js with css external

--- src/App.js
+++ src/App.js
@@ -22,14 +22,24 @@ import React from 'react'
 //   )
 // }
 
+// function App() {
+//   const divStyle = {
+//     color: 'red',
+//     backgroundColor: 'powderblue',
+//     fontSize: 'larger',
+//     padding: '3px',
+//   }
+//   return <div style={divStyle}>Hello React</div>
+// }
+
+import './style.css'
 function App() {
-  const divStyle = {
-    color: 'red',
-    backgroundColor: 'powderblue',
-    fontSize: 'larger',
-    padding: '3px',
-  }
-  return <div style={divStyle}>Hello React</div>
+return (
+  <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+  </>
+)
 }
 
 export default App
