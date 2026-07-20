44821c850a404eee5ae33ffaf623ab0467edb49e
Sennnss <marionvam723@gmail.com>
2026-07-20 10:54:52 +0700

    add class compo
    add class compo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Header, Content, Footer } from './func-components'
-
+import Calender from './class-components'
 function App() {
   return (
     /*[<Header />, <Content />, <Footer />] */
     <div>
       <Header />
+      <p><center><Calender /></center></p>
       <Content />
       <Footer />
     </div>
