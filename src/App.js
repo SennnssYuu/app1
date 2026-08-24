2c39a9fbf2002102012bb5591258f45cbe7246aa
Sennnss <marionvam723@gmail.com>
2026-08-24 09:25:27 +0700

    Add Effect.js
    Add Effect.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import MessageBox from './state-func'
+import Effect from './effect';
 function App() {
-   return <MessageBox/> 
+   return <Effect/>
 }
-export default  App;
+export default App;
